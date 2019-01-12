e5a08c072b0d5dfe16af41f959f7356055d2f3a7
chenj209 <kinderjianda.chen@gmail.com>
2019-01-12 14:41:30 -0500

    progress bar

--- Survive! Tony!/Assets/Scripts/Tile.cs
+++ Survive! Tony!/Assets/Scripts/Tile.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Slider progressbar;
+    void OnMouseDown() {
+        progressbar.gameObject.SetActive(true);
+        progressbar.value = 0.1f;
     }
 }
