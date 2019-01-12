89cb03048c577b05596cd27271dc50ff2d784325
Kennnyoung <kyh649782182@gmail.com>
2019-01-12 13:23:20 -0500

    added prototype

--- Survive! Tony!/Assets/Scripts/Tile.cs
+++ Survive! Tony!/Assets/Scripts/Tile.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
-{
+{ 
+    public int progress;
+    private bool pressed = false;
+    public TileType type;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        if (Input.GetMouseButtonDown(0))
+        {
+            pressed = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            pressed = false;
+        }
+        UpdateProgress();
+    }
+
+    void UpdateProgress()
+    {
+        if (pressed)
+        {
+            progress += 1;
+        }
+    }
+
+    void UpdateResource()
     {
         
     }
 }
+
+public enum TileType {empty, wall, resources, trap, pipe}
